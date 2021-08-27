2752f9021abb7b90e926bdd446a7c99290dbce54
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-08-27 17:08:00 +0300

    Add 404 error page

---
+++ portfolio/pages/404.js
@@ -0,0 +1,77 @@
+import styles from '../styles/Error.module.scss'
+import Head from 'next/head'
+import Lottie from 'react-lottie';
+import animation from '../public/404_animation.json';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Controller, Scene } from "react-scrollmagic";
+import { ToastContainer } from 'react-nextjs-toast';
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+
+//	Declare output function
+function Error () {
+
+	/* ================================================ Output final html =============================================== */
+	return (
+		<div className={styles.container}>
+
+			{/* Head */}
+			<Head>
+
+				{/* Details */}
+				<title>Page Not Found | Muhammad Fauzan Aristya Putra</title>
+				<meta name="description" content="Muhammad Fauzan Aristya Putra's Portfolio" />
+				<link rel="icon" href="/favicon.ico" />
+
+			</Head>
+
+			{/* Homepage */}
+			<div className={styles.error}>
+
+				{/* ====================================================== Body ====================================================== */}
+				<div className={styles.body}>
+
+					{/* Animation */}
+					<div className={styles.animation}><Lottie options={{
+						loop			: true,
+						autoPlay		: true,
+						animationData		: animation,
+						rendererSettings	: {
+							className		: styles.player,
+							progressiveLoad		: true
+						}
+					}}/></div>
+
+					{/* Header */}
+					<div className={styles.header}>PAGE NOT FOUND</div>
+
+					{/* Description */}
+					<div className={styles.description}>
+						Unfortunately this page does not exist. Please click the link <br/> below to return home.
+					</div>
+
+					{/* Call to Action */}
+					<div className={styles.call_to_action}>Return Home</div>
+
+				</div>
+
+
+
+				{/* ===================================================== Footer ===================================================== */}
+				<Footer/>
+				
+
+
+				{/* ================================================== Miscellaneous ================================================= */}
+
+				{/* Navbar */}
+				<div className="navbar_container"><Navbar pathname="error"/></div>
+
+			</div>
+
+		</div>
+	);
+
+}
+
+export default Error
