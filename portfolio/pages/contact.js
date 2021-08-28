4d1ff82ffcdda04f0599f00354c2871377928db1
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-08-28 19:05:09 +0300

    Add barebones contact page

---
+++ portfolio/pages/contact.js
@@ -0,0 +1,50 @@
+import styles from '../styles/Contact.module.scss'
+import Head from 'next/head'
+import Lottie from 'react-lottie';
+import animation from '../public/404_animation.json';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Controller, Scene } from "react-scrollmagic";
+import { ToastContainer } from 'react-nextjs-toast';
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+
+//	Declare output function
+function Contact () {
+
+	/* ================================================ Output final html =============================================== */
+	return (
+		<div className={styles.container}>
+
+			{/* Head */}
+			<Head>
+
+				{/* Details */}
+				<title>Contact | Muhammad Fauzan Aristya Putra</title>
+				<meta name="description" content="Muhammad Fauzan Aristya Putra's Portfolio" />
+				<link rel="icon" href="/favicon.ico" />
+
+			</Head>
+
+			{/* Homepage */}
+			<div className={styles.contact}>
+
+
+
+				{/* ===================================================== Footer ===================================================== */}
+				<Footer/>
+				
+
+
+				{/* ================================================== Miscellaneous ================================================= */}
+
+				{/* Navbar */}
+				<div className="navbar_container"><Navbar pathname="contact"/></div>
+
+			</div>
+
+		</div>
+	);
+
+}
+
+export default Contact
