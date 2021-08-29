e6939800e8bbc2c0782198bbbd367541711e3f79
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-08-29 13:16:58 +0300

    Fix bug with copying email to clipboard

--- portfolio/pages/utils/copy_to_clipboard.js
+++ portfolio/pages/utils/copy_to_clipboard.js
@@ -1,14 +1,22 @@
-import React, { useState, useEffect } from 'react';
 import { toast } from 'react-nextjs-toast';
 
 //	Function used to copy data to clipboard
 function CopyToClipboard (data, message = '') {
 
-	//	Use Effect
-	useEffect(() => {
+	//	Check if this function is being run in the dom
+	if (typeof window !== 'undefined') {
+
+		//	Create new text area to hold text to copy
+		const text = document.createElement('textarea');
+		text.value = data;
+
+		//	Add text area to dom
+		document.body.appendChild(text);
 
 		//	Copy to clipboard
-		navigator.clipboard.writeText(data);
+		text.select();
+		document.execCommand('copy');
+		document.body.removeChild(text);
 
 		//	Show toast
 		toast.notify(`${message} Copied to Clipboard`, {
@@ -16,10 +24,7 @@ function CopyToClipboard (data, message = '') {
 			title		: 'Success',
 		});
 
-		//	Return nothing
-		return null; 
-
-	});
+	}
 
 	//	Return nothing
 	return null; 
