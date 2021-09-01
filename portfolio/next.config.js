18117b948bf3bdfb049a0fa10019eb67f036737d
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-09-01 18:33:20 +0300

    Tweak next js config

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,9 +1,5 @@
 const config = {
         reactStrictMode         : true,
-        onDemandEntries         : {
-                maxInactiveAge          : 60 * 1000,
-                pagesBufferLength       : 5,
-        }
 };
 
 module.exports = config
