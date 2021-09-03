33cf4317cc61e481dd72ffaf05efe3941e712a42
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-09-03 16:36:24 +0300

    Create accordion component

---
+++ portfolio/components/accordion.js
@@ -0,0 +1,75 @@
+/* ------------------------------------------------------------------------------------------------------------------ */
+/*                                                       Imports                                                      */
+/* ------------------------------------------------------------------------------------------------------------------ */
+import React, { createRef, useRef } from 'react';
+import Image from 'next/image';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Controller, Scene } from "react-scrollmagic";
+
+
+
+
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+/*                                                   Accordion Class                                                  */
+/* ------------------------------------------------------------------------------------------------------------------ */
+class Accordion extends React.Component {
+
+        /* ===================================================== On Load ==================================================== */
+	constructor (props) {
+		super(props);
+
+		//	Set state
+		this.state = {
+                        open                            : false,
+			title				: props.options ? (props.options.title || 'Accordion') : 'Accordion',
+		}
+
+	}
+
+
+
+        /* ==================================================== Functions =================================================== */
+
+	/* Toggle */
+	toggle () {
+
+		//	Set open state
+                this.setState({open: !this.state.open});
+
+	}
+
+
+
+	/* ==================================================== On Render =================================================== */
+	render () {
+
+                //      Generate class name
+                var class_name = this.state.open ? `active accordion` : `accordion`;
+
+		//	Return html
+		return (
+                        <div className={class_name}>
+
+                                {/* Title */}
+                                <div className="title" onClick={this.toggle.bind(this)}>
+
+                                        {/* Text */}
+                                        {this.state.title}
+
+                                        {/* Icon */}
+                                        <FontAwesomeIcon className="chevron" icon={['fas', 'chevron-down']}/>
+
+                                </div>
+
+                                {/* Content */}
+                                <div className="content">{this.props.children}</div>
+
+                        </div>
+                );
+
+	}
+
+}
+
+export default Accordion
