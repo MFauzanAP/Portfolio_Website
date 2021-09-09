6841f28245f09ab98af992e6535cc4e4d8924f3a
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-09-09 17:15:47 +0300

    Add get projects api route

---
+++ portfolio/pages/api/project/get_projects.js
@@ -0,0 +1,16 @@
+//      Imports
+import { connectToDatabase } from '../../../middleware/mongodb';
+
+//	Function used to retrieve projects from the database
+export default async function (req, res) {
+
+	//      Connect to database
+	const { db } = await connectToDatabase();
+
+	//      Get collection data
+	const projects = await db.collection('projects').find({}).toArray();
+
+	//      Return projects
+	res.json(projects);
+
+}
