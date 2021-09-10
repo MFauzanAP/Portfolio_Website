56351cdc9f5cb192f8c2c80d88ad6877afbd4fda
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-09-10 12:43:12 +0300

    Disable project model temporarily

--- portfolio/models/project.js
+++ portfolio/models/project.js
@@ -1,40 +1,40 @@
-//      Imports
-import mongoose from 'mongoose';
-var Schema = mongoose.Schema;
+// //      Imports
+// import mongoose from 'mongoose';
+// var Schema = mongoose.Schema;
 
-//      Declare project model
-var project = new Schema({
-	name			: {
-		type			: String,
-		required		: true
-	},
-	images			: [{
-		type			: String,
-		required		: true
-	}],
-	description		: {
-		short			: {
-			type			: String,
-			required		: true
-		}, 
-		long			: {
-			type			: String,
-			required		: true
-		}
-	},
-	tech_stack		: [{
-		type			: String,
-		required		: true
-	}],
-	date			: {
-		type			: Date,
-		required		: true
-	}
-})
+// //      Declare project model
+// var project = new Schema({
+// 	name			: {
+// 		type			: String,
+// 		required		: true
+// 	},
+// 	images			: [{
+// 		type			: String,
+// 		required		: true
+// 	}],
+// 	description		: {
+// 		short			: {
+// 			type			: String,
+// 			required		: true
+// 		}, 
+// 		long			: {
+// 			type			: String,
+// 			required		: true
+// 		}
+// 	},
+// 	tech_stack		: [{
+// 		type			: String,
+// 		required		: true
+// 	}],
+// 	date			: {
+// 		type			: Date,
+// 		required		: true
+// 	}
+// })
 
-//	Add model to database
-mongoose.models = {};
-var Project = mongoose.model('Project', project);
+// //	Add model to database
+// mongoose.models = {};
+// var Project = mongoose.model('Project', project);
 
-//	Export model
-export default Project
+// //	Export model
+// export default Project
