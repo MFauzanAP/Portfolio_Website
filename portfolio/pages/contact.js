49d3d162fc95adbcccf6790d8e60ef680b1531a3
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-09-14 08:07:32 +0300

    Add another question to faq section

--- portfolio/pages/contact.js
+++ portfolio/pages/contact.js
@@ -280,6 +280,13 @@ function Contact () {
 									Click here to <Link href="/about#tech_stack"><a>view a list of the languages and tools I use. </a></Link>
 								</p>
 							</Accordion></div>
+							<div className={styles.accordion} style={{transitionDelay: '2.55s'}}><Accordion options={{title: 'How can I check your work?'}}>
+								<p>
+									<Link href="/projects"><a>Click here to view my projects. </a></Link>
+									If you'd like to check out the quality of my code or how frequently I work then
+									<a href="https://github.com/MFauzanAP" target="_blank" rel="noreferrer"> click here to visit my GitHub profile.</a>
+								</p>
+							</Accordion></div>
 
 						</div>
 
