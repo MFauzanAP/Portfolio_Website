4267a89040a8c5c96836008fe4ed420251e192da
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2021-09-23 11:59:11 +0300

    Add loading screen component

---
+++ portfolio/components/loader.js
@@ -0,0 +1,74 @@
+/* ------------------------------------------------------------------------------------------------------------------ */
+/*                                                       Imports                                                      */
+/* ------------------------------------------------------------------------------------------------------------------ */
+import React from 'react';
+import { motion } from 'framer-motion';
+
+
+
+
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+/*                                                    Loader Class                                                    */
+/* ------------------------------------------------------------------------------------------------------------------ */
+class Loader extends React.Component {
+
+	/* ==================================================== On Render =================================================== */
+	render () {
+
+		//	Return html
+		return (
+			<motion.div className="loader" exit="initial" initial="initial" animate="animate" variants={{
+					animate		: {
+						width		: '0%',
+						height		: '0%',
+						left		: '50%',
+						top		: '50%',
+						transition: {
+							duration	: 1.5,
+							ease		: [0.87, 0, 0.13, 1],
+						},
+					},
+					initial		: {
+						width		: '300%',
+						height		: '300%',
+						left		: '-100%',
+						top		: '-100%',
+						transition: {
+							duration	: 1.5,
+							ease		: [0.87, 0, 0.13, 1],
+						},
+					}
+				}}>
+
+				{/* Sections */}
+				<div className="blob">
+
+					{/* Indicators */}
+					<motion.div className="indicators" exit="initial" initial="initial" animate="animate" variants={{
+							animate		: {
+								opacity		: 0
+							},
+							initial		: {
+								opacity		: 1,
+								transition: {
+									duration	: 1,
+									ease		: [0.87, 0, 0.13, 1],
+								},
+							}
+						}}>
+						<div className="indicator"></div>
+						<div className="indicator"></div>
+						<div className="indicator"></div>
+					</motion.div>
+
+				</div>
+
+			</motion.div>
+		);
+
+	}
+
+}
+
+export default Loader
